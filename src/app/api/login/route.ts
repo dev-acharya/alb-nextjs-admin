c7dfb2f98e397f3d47c5258f6665c435ad8d1025
kartik-webDev <kartik.microservices.dev@gmail.com>
2025-12-04 16:26:06 +0530

    increase the age of cookie

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -30,7 +30,7 @@ export async function POST(request: Request) {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'strict',
-      maxAge: 3600, // 1 hour
+      maxAge: 60 * 60 * 24 * 7, // 1 week
       path: '/',
     });
 
