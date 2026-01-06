02612848e03bdbdb2fad822a0fe7f2de2c87cbeb
kartik-webDev <kartik.microservices.dev@gmail.com>
2026-01-06 10:36:37 +0530

    hide tabs those are not ready yet

--- src/lib/route.tsx
+++ src/lib/route.tsx
@@ -55,21 +55,21 @@ export const RouteName = [
         name: "List Of Astrologers",
         icon: <OtherRouteSvg />,
       },
-      {
-        path: "/astrologer-enquiry",
-        name: "Astrologer Enquiry",
-        icon: <OtherRouteSvg />,
-      },
-      {
-        path: "/astrologer-issues",
-        name: "Astrologer Issues",
-        icon: <OtherRouteSvg />,
-      },
-      {
-        path: "/leave-request",
-        name: "Leave request",
-        icon: <OtherRouteSvg />,
-      },
+      // {
+      //   path: "/astrologer-enquiry",
+      //   name: "Astrologer Enquiry",
+      //   icon: <OtherRouteSvg />,
+      // },
+      // {
+      //   path: "/astrologer-issues",
+      //   name: "Astrologer Issues",
+      //   icon: <OtherRouteSvg />,
+      // },
+      // {
+      //   path: "/leave-request",
+      //   name: "Leave request",
+      //   icon: <OtherRouteSvg />,
+      // },
     ],
   },
   {
@@ -93,38 +93,38 @@ export const RouteName = [
       // }
     ],
   },
-  {
-    name: "Live Session",
-    icon: <LiveRouteSvg />,
-    subRoutes: [
-      {
-        path: "/live-session/category",
-        name: "Category",
-        icon: <OtherRouteSvg />,
-      },
-      {
-        path: "/live-session/topic",
-        name: "Topic",
-        icon: <OtherRouteSvg />,
-      },
-    ],
-  },
-  {
-    name: "Notification",
-    icon: <NotificationRouteSvg />,
-    subRoutes: [
-      {
-        path: "/notification/customer-notification",
-        name: "Customer Notification",
-        icon: <OtherRouteSvg />,
-      },
-      {
-        path: "/notification/astrologer-notification",
-        name: "Astrologer Notification",
-        icon: <OtherRouteSvg />,
-      },
-    ],
-  },
+  // {
+  //   name: "Live Session",
+  //   icon: <LiveRouteSvg />,
+  //   subRoutes: [
+  //     {
+  //       path: "/live-session/category",
+  //       name: "Category",
+  //       icon: <OtherRouteSvg />,
+  //     },
+  //     {
+  //       path: "/live-session/topic",
+  //       name: "Topic",
+  //       icon: <OtherRouteSvg />,
+  //     },
+  //   ],
+  // },
+  // {
+  //   name: "Notification",
+  //   icon: <NotificationRouteSvg />,
+  //   subRoutes: [
+  //     {
+  //       path: "/notification/customer-notification",
+  //       name: "Customer Notification",
+  //       icon: <OtherRouteSvg />,
+  //     },
+  //     {
+  //       path: "/notification/astrologer-notification",
+  //       name: "Astrologer Notification",
+  //       icon: <OtherRouteSvg />,
+  //     },
+  //   ],
+  // },
   // {
   //   path: "/consultation",
   //   name: "Consultation",
@@ -152,41 +152,41 @@ export const RouteName = [
     ],
   },
 
-  {
-    path: "/skill",
-    name: "Skill",
-    icon: <SkillRouteSvg />,
-  },
-  {
-    path: "/remedies",
-    name: "Remedies",
-    icon: <RemediesRouteSvg />,
-  },
-  {
-    path: "/main-expertise",
-    name: "Main Expertise",
-    icon: <MainExpertiesRouteSvg />,
-  },
-  {
-    path: "/gift",
-    name: "Gift",
-    icon: <GiftRouteSvg />,
-  },
-  {
-    path: "/banner",
-    name: "Banner",
-    icon: <BannerRouteSvg />,
-  },
-  {
-    path: "/recharge",
-    name: "Recharge",
-    icon: <RechargeRouteSvg />,
-  },
-  {
-    path: "/review",
-    name: "Review",
-    icon: <RatingRouteSvg />,
-  },
+  // {
+  //   path: "/skill",
+  //   name: "Skill",
+  //   icon: <SkillRouteSvg />,
+  // },
+  // {
+  //   path: "/remedies",
+  //   name: "Remedies",
+  //   icon: <RemediesRouteSvg />,
+  // },
+  // {
+  //   path: "/main-expertise",
+  //   name: "Main Expertise",
+  //   icon: <MainExpertiesRouteSvg />,
+  // },
+  // {
+  //   path: "/gift",
+  //   name: "Gift",
+  //   icon: <GiftRouteSvg />,
+  // },
+  // {
+  //   path: "/banner",
+  //   name: "Banner",
+  //   icon: <BannerRouteSvg />,
+  // },
+  // {
+  //   path: "/recharge",
+  //   name: "Recharge",
+  //   icon: <RechargeRouteSvg />,
+  // },
+  // {
+  //   path: "/review",
+  //   name: "Review",
+  //   icon: <RatingRouteSvg />,
+  // },
   {
     name: "Astroblog",
     icon: <BlogsRouteSvg />,
@@ -203,78 +203,78 @@ export const RouteName = [
       },
     ],
   },
-  {
-    name: "Master",
-    icon: <OtherRouteSvg />,
-    subRoutes: [
-      {
-        path: "/master/slot-management",
-        name: "Slot Management",
-        icon: <OtherRouteSvg />,
-      },
-      {
-        path: "/master/platform-charges",
-        name: "Platform Charge",
-        icon: <OtherRouteSvg />,
-      },
-      {
-        path: "/master/predefined-message",
-        name: "Predefined Message",
-        icon: <OtherRouteSvg />,
-      },
-    ],
-  },
-  {
-    path: "/pages",
-    name: "Pages",
-    icon: <OtherRouteSvg />,
-    subRoutes: [
-      {
-        path: "/pages/terms-and-conditions",
-        name: "Terms and Conditions",
-        icon: <OtherRouteSvg />,
-      },
-      {
-        path: "/pages/privacy-policy",
-        name: "Privacy Policy",
-        icon: <OtherRouteSvg />,
-      },
-      {
-        path: "/pages/about-us",
-        name: "About Us",
-        icon: <OtherRouteSvg />,
-      },
-      // {
-      //   path: "/displayHowToUse",
-      //   name: "How to use- ScreenShots",
-      //   icon: <OtherRouteSvg />,
-      // },
-      // {
-      //   path: "/displayHowToUseVideos",
-      //   name: "How to use - Videos",
-      //   icon: <OtherRouteSvg />,
-      // },
-    ],
-  },
-  {
-    path: "/reports/admin-earning",
-    name: "Admin Earning",
-    icon: <OtherRouteSvg />,
-  },
+  // {
+  //   name: "Master",
+  //   icon: <OtherRouteSvg />,
+  //   subRoutes: [
+  //     {
+  //       path: "/master/slot-management",
+  //       name: "Slot Management",
+  //       icon: <OtherRouteSvg />,
+  //     },
+  //     {
+  //       path: "/master/platform-charges",
+  //       name: "Platform Charge",
+  //       icon: <OtherRouteSvg />,
+  //     },
+  //     {
+  //       path: "/master/predefined-message",
+  //       name: "Predefined Message",
+  //       icon: <OtherRouteSvg />,
+  //     },
+  //   ],
+  // },
+  // {
+  //   path: "/pages",
+  //   name: "Pages",
+  //   icon: <OtherRouteSvg />,
+  //   subRoutes: [
+  //     {
+  //       path: "/pages/terms-and-conditions",
+  //       name: "Terms and Conditions",
+  //       icon: <OtherRouteSvg />,
+  //     },
+  //     {
+  //       path: "/pages/privacy-policy",
+  //       name: "Privacy Policy",
+  //       icon: <OtherRouteSvg />,
+  //     },
+  //     {
+  //       path: "/pages/about-us",
+  //       name: "About Us",
+  //       icon: <OtherRouteSvg />,
+  //     },
+  //     // {
+  //     //   path: "/displayHowToUse",
+  //     //   name: "How to use- ScreenShots",
+  //     //   icon: <OtherRouteSvg />,
+  //     // },
+  //     // {
+  //     //   path: "/displayHowToUseVideos",
+  //     //   name: "How to use - Videos",
+  //     //   icon: <OtherRouteSvg />,
+  //     // },
+  //   ],
+  // },
+  // {
+  //   path: "/reports/admin-earning",
+  //   name: "Admin Earning",
+  //   icon: <OtherRouteSvg />,
+  // },
   {
     path: "/reports/report-orders",
     name: "Report Order",
     icon: <OtherRouteSvg />,
 
   },
-  {
-    path: "/language",
-    name: "Language",
-    icon: <LanguageRouteSvg />,
-  },
-  {
-    path: "/announcement",
-    name: "Announcement",
-    icon: <AnnouncementRouteSvg/>,
-  },
+  // {
+  //   path: "/language",
+  //   name: "Language",
+  //   icon: <LanguageRouteSvg />,
+  // },
+  // {
+  //   path: "/announcement",
+  //   name: "Announcement",
+  //   icon: <AnnouncementRouteSvg/>,
+  // },
 ];
